3a2e956ec09de12b486b49f1dbce5540b4c9327e
bennyp <53509098+ultrabeney@users.noreply.github.com>
2022-04-08 14:42:58 +0700

    Creating Fragment

--- app/src/main/java/com/example/trackyourtools/MainActivity.java
+++ app/src/main/java/com/example/trackyourtools/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.trackyourtools;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.trackyourtools.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.Home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.Location:
+                    replaceFragment(new MidFragment());
+                    break;
+                case R.id.List:
+                    replaceFragment(new EndFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.FrameLayout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
